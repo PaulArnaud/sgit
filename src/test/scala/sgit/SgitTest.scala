719b3da80117f13e0140525b781fd3c30c4adb1c
PaulArnaud <paul.arnaud@etu.umontpellier.fr>
2019-10-05 11:05:46 +0200

    Second Test

--- src/test/scala/sgit/SgitTest.scala
+++ src/test/scala/sgit/SgitTest.scala
@@ -2,9 +2,10 @@ package sgit
 
 import org.scalatest._
 
-class SgitTest extends FunSpec with Matchers {
-
-    describe("Print Hello") {
-        assert(1==1)
-    }
+class SgitTest extends FunSpec with FlatSpec with Matchers {
+  "Directory" should "be create" in {
+    new File(".sgit")
+    assert(new File(".sgit").exists())
+  }
+}
 }
