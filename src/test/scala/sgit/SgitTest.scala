0a5f33db2d06c2b6563cc2e55fd8dc4e2d032f30
PaulArnaud <paul.arnaud@etu.umontpellier.fr>
2019-10-05 11:11:23 +0200

    Fourth Test

--- src/test/scala/sgit/SgitTest.scala
+++ src/test/scala/sgit/SgitTest.scala
@@ -3,7 +3,7 @@ package sgit
 import java.io.File
 import org.scalatest._
 
-class SgitTest extends FunSpec with FlatSpec with Matchers {
+class SgitTest extends FlatSpec with Matchers {
   "Directory" should "be create" in {
     new File(".sgit")
     assert(new File(".sgit").exists())
