2269e689719479820c917baee2d84416d3144b92
PaulArnaud <paul.arnaud@etu.umontpellier.fr>
2019-10-05 11:00:40 +0200

    First Test

---
+++ src/main/scala/sgit/Sgit.scala
@@ -0,0 +1,7 @@
+package sgit 
+
+object Sgit {
+    def main(args: Array[String]): Unit = {
+        println("Hello from main of object")
+    }
+}
---
+++ src/test/scala/sgit/SgitTest.scala
@@ -0,0 +1,10 @@
+package sgit
+
+import org.scalatest._
+
+class SgitTest extends FunSpec with Matchers {
+
+    describe("Print Hello") {
+        assert(1==1)
+    }
+}
