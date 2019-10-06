2c994ad2a88af932199dae4a7b8113d6338e24ca
PaulArnaud <paul.arnaud@etu.umontpellier.fr>
2019-10-06 18:11:25 +0200

    Try add

--- src/main/scala/sgit/FileTools.scala
+++ src/main/scala/sgit/FileTools.scala
@@ -42,16 +42,34 @@ object FileTools {
             .createIfNotExists()
     }
 
-    def createBlop() : Unit = {
+    def createBlop(file :JavaFile) : Unit = {
+        // Content of a file
+        /*
+        creation du sha1 avec le nom du fichier
+        creation d'un fichier de nom (clé en sha1) et de contenu lefichier d'entrée
+        */
 
     }
 
     def createTree() : Unit = {
+        // one or several reference to blop OR one or several tree
+        /*  
+        De la forme : TYPE SHA1 nomFICHIER ||  Exemple : blob cf59e02c3d2a2413e2da9e535d3c116af1077906 README.md
+
+
+        */
 
     }
 
     def createCommit() : Unit = {
+        // one or many tree, one or many parent (commit) 
+        /*  
+        De la forme : 
+        tree f89e64bdfcc08a8b371ee76a74775cfe096655ce
+        author zspajich <zspajich@gmail.com> 1516710703 +0100
+        committer zspajich <zspajich@gmail.com> 1516710703 +0100Initial Commit
 
+        */
     }
 
     def addToStage(file: JavaFile) : Unit = {
@@ -59,5 +77,45 @@ object FileTools {
         val fw = new FileWriter(STAGE,true)
         fw.write(file.getAbsolutePath() + "\n")
         fw.close()
+    }
+    
+    def findHome() : JavaFile = {
+        return new JavaFile("")
+    }
+
+    def hasFindRepo() : Boolean = {
+        return true
+    }
+
+    def recursiveListFiles(f: JavaFile): Array[JavaFile] = {
+        val these = f.listFiles
+        these ++ these.filter(_.isDirectory).flatMap(recursiveListFiles)
+    }
+
+    def isTracked(f: JavaFile) : Boolean = {
+        return true
+    }
+
+    def isModified(f: JavaFile) : Boolean = {
+        val blop = findTheCorrespondingBlop()
+        val hasBeenModified = false
+
+        /* if (blop.length == f.length) {
+            for i in 0, blop.length {
+                if (blop[i] != f[i]) {
+                    hasBeenModified = true
+                }
+            }
+        }
+        else {
+            hasBeenModified = true
+        }
+
+        */
+        return true
+    }
+
+    def findTheCorrespondingBlop() : JavaFile = {
+        return new JavaFile("")
     }
 }
--- src/main/scala/sgit/Options.scala
+++ src/main/scala/sgit/Options.scala
@@ -14,7 +14,27 @@ object Options {
     }
 
     def diff() : Unit = {
+        if (FileTools.hasFindRepo()) {
+            val homeDir = FileTools.findHome()
+            val (tracked, untracked) = FileTools.recursiveListFiles(homeDir).span(x => FileTools.isTracked(x))
+            val (modified, unmodified) = tracked.span(x => FileTools.isModified(x))
 
+            untracked.foreach(x => println("The file : " + x.getAbsolutePath() + " is untracked"))
+            modified.foreach(x => println("The file : " + x.getAbsolutePath() + " has been modified"))
+        }
+        else {
+            println("No repo")
+        }
+        /*
+        pour chaque élément au niveau du .sgit : 
+            si élément = dossier alors :
+                récursion sur le contenu du dossier
+            si élément = fichier alors :
+                si élément est tracké alors :
+                    on regarde si il est modifié 
+                sinon : 
+                    on le track
+        */
     }
 
     def add(files: Seq[File]) : Unit = {
